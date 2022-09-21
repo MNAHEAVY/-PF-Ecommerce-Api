67f1d61161d9210444367a8433f19dbe42fce93f
JoaquinAngelino <joacoangelino@gmail.com>
2022-09-21 17:34:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {seederMarcas}=require('./src/Seeders/marca.seeders')
@@ -25,9 +6,9 @@ const {productoSeeder}=require('./src/Seeders/producto.seeders')
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
-    seederMarcas();
-    productoSeeder();
-    seederRole();  
+    // seederMarcas();
+    // productoSeeder();
+    // seederRole();  
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
