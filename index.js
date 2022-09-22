9fcc8dd3941ef3542d4c875c98b15495e30c30ac
Martín Araujo <98348984+MNAHEAVY@users.noreply.github.com>
2022-09-21 22:34:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const {seederMarcas}=require('./src/Seeders/marca.seeders')
 const {seederRole}=require('./src/Seeders/role.seeders')
 const {productoSeeder}=require('./src/Seeders/producto.seeders')
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
-    // seederMarcas();
-    // productoSeeder();
-    // seederRole();  
+    seederMarcas();
+    productoSeeder();
+    seederRole();  
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
